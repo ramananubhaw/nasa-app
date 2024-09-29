623803e31f0f213a2216c7b53efacc9d8d33fd9a
Anubhaw Raman <anubhawraman@gmail.com>
2024-09-29 10:05:58 +0530

    Minor improvements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,35 @@
+import {useRef, useEffect, useState} from 'react';
+
 export default function Main(props) {
 
   const {data, showModal, handleToggleModal} = props;
+  const containerRef = useRef(null);
+  const imageRef = useRef(null);
+  const [imageWidth, setImageWidth] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      if (containerRef.current && imageRef.current) {
+        setContainerWidth(containerRef.current.offsetWidth);
+        setImageWidth(imageRef.current.offsetWidth)
+      }
+    };
+
+    if (imageRef.current) {
+      imageRef.current.onload = updateDimensions;
+    }
+
+    return () => {
+      if (imageRef.current) {
+        imageRef.current.onload = null;
+      }
+    };
+  }, [data]);
 
   return (
-    <div onClick={() => {if (showModal) handleToggleModal();}} className="imgContainer">
-      <img src={data.hdurl} alt={data.title || 'APOD Image'} className="bgImage" />
+    <div onClick={() => {if (showModal) handleToggleModal();}} className="imgContainer" ref={containerRef}>
+      <img src={data.hdurl} alt={data.title || 'APOD Image'} className="bgImage" ref={imageRef} />
     </div>
     
   )
